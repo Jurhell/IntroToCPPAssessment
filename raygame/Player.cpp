32499c50765194342dd6557509e9308e24fc0375
Paris Rainey <paris.c.rainey@gmail.com>
2024-01-08 12:56:07 -0600

    Began Adding Functions to Player Class
    Added On Collision function to Player,
    Added constructor to player,
    Added Update to player,
    Added speed to Player

--- raygame/Player.cpp
+++ raygame/Player.cpp
@@ -1,2 +1,8 @@
+#include "Player.h"
 #include "Actor.h"
+#include "Transform2D.h"
 
+Player::Player(const char* spritepath, Vector2 positionX, Vector2 positionY)
+{
+
+};
--- raygame/Player.h
+++ raygame/Player.h
@@ -1,9 +1,15 @@
 #include "Actor.h"
+#include "Transform2D.h"
 
 class Player
 {
 public:
-	Player(const char* spritepath, )
+	Player(const char* spritepath, Vector2 positionX, Vector2 positionY);
+
+	void OnCollision(Actor other);
+
+	void Update(float deltaTime);
+
 private:
 	float m_speed;
 
