828819075aa058fcf24eec6c99d86dd1ce1a0637
Paris Rainey <paris.c.rainey@gmail.com>
2024-01-10 14:57:29 -0600

    Filled Out Functions in Player Class
    Added WASD movement to player,
    Added Sprite Component to player,
    Added direction to player,
    Added health to player

--- raygame/Player.h
+++ raygame/Player.h
@@ -1,16 +1,24 @@
 #include "Actor.h"
 #include "Transform2D.h"
 
+
 class Player
 {
 public:
-	Player(const char* spritepath, Vector2 positionX, Vector2 positionY);
+	Player(const char* spritepath, float speed, float health, MathLibrary::Vector2 position);
+	~Player();
 
+	/// <summary>
+	/// Will calaculate health/life loss on collsion with another object.
+	/// </summary>
 	void OnCollision(Actor other);
 
+	/// <summary>
+	/// Contains button attachment to movement and shooting along with speed/velocity management.
+	/// </summary>
 	void Update(float deltaTime);
 
 private:
 	float m_speed;
-
+	float m_health;
 };
